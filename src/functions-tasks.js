28b97905d5d59eff15f13afd3c024193f3c41de7
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2025-04-13 18:18:00 +0300

    feat: implement logger

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -175,8 +175,15 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function ret(...args) {
+    const argsStr = args.map((arg) => JSON.stringify(arg)).join(',');
+    const funcName = func.name;
+    logFunc(`${funcName}(${argsStr}) starts`);
+    const result = func(...args);
+    logFunc(`${funcName}(${argsStr}) ends`);
+    return result;
+  };
 }
 
 /**
