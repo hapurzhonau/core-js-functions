d80ac59492033fc5cf486960c6e9cadf0694f012
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2025-04-13 17:42:00 +0300

    feat: implement retry

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -139,8 +139,17 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function ret() {
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (error) {
+        if (i === attempts - 1) throw error;
+      }
+    }
+    return func();
+  };
 }
 
 /**
